ac4a0c0c1d9e3e97b37352764026801a0f507a9c
Bel29 <ihara.belen.olivera@fing.edu.uy>
2022-11-05 18:10:55 -0300

    update

--- obligatorio.asm
+++ obligatorio.asm
@@ -1,316 +1,297 @@
 .data
-#define DS 100h
-;;----------Constantes------------------------------------------
+
+#define DS 0
+
+;;----------  Constantes  --------------------------------------
 ;;--------------------------------------------------------------
 
-PUERTO_ENTRADA EQU 10
+ENTRADA EQU 10
 PUERTO_SALIDA_DEFECTO EQU 1
 PUERTO_LOG_DEFECTO EQU 2
 STACK_SIZE EQU 31
 
-;;----------Variables------------------------------------------
-;;--------------------------------------------------------------
+;;----------  Variables  --------------------------------------
+;;-------------------------------------------------------------
 
 stack DW DUP(STACK_SIZE) 0
-tope dw 0  
+tope DW 0  
+puertoLog DW PUERTO_LOG_DEFECTO
+puertoSalida DW PUERTO_SALIDA_DEFECTO
+
+;;---------  Constante Auxiliar  -------------------------------
+;;--------------------------------------------------------------
 						
 base equ stack 
-
-puertoLog dw PUERTO_LOG_DEFECTO
-puertoSalida dw PUERTO_SALIDA_DEFECTO
 
 .code
 
 main:
-	mov dx, [puertoLog]
-	mov ax, 0
-	out dx, ax					
-	in ax, PUERTO_ENTRADA									
-	out dx, ax					
+	mov DX, [puertoLog] 
+	mov AX, 0			
+	out DX, AX							; Muestro el 0 en puertoLog		
+	in AX, ENTRADA						; AX = codigo						
+	out DX, AX							; Lo muestro en el puerto		
 	
-	cmp ax,1
+	cmp AX,1
 	je NUM
-	cmp ax,2
+	cmp AX,2
 	je PORT
-	cmp ax,3
+	cmp AX,3
 	je LOG
-	cmp ax,4
+	cmp AX,4
 	je TOP
-	cmp ax,5
+	cmp AX,5
 	je DUMP
-	cmp ax,6
+	cmp AX,6
 	je DUPP
-	cmp ax,7
+	cmp AX,7
 	je SWAP
-	cmp ax,8
+	cmp AX,8
 	je OPPOSITE
-	cmp ax,9
+	cmp AX,9
 	je FACT
-	cmp ax,10
+	cmp AX,10
 	je SUM_ALL
-	cmp ax,11
+	cmp AX,11
 	je SUM_ONE
-	cmp ax,12
+	cmp AX,12
 	je SUBB
-	cmp ax,13
+	cmp AX,13
 	je MULTIPLY
-	cmp ax,14
+	cmp AX,14
 	je DIVV
-	cmp ax,15
+	cmp AX,15
 	je MOD
-	cmp ax,16
+	cmp AX,16
 	je ANDD
-	cmp ax,17
+	cmp AX,17
 	je ORR
-	cmp ax,18
+	cmp AX,18
 	je LEFTS
-	cmp ax,19
+	cmp AX,19
 	je RIGHTS
-	cmp ax,254
+	cmp AX,254
 	je CLEAR
-	cmp ax,255
+	cmp AX,255
 	je HALT
 	jmp comandoInvalido
 
 
 comandoInvalido:
-	mov ax,2
-	mov dx, [puertoLog]
-	out dx, ax	
+	mov AX,2
+	mov DX, [puertoLog]
+	out DX, AX	
 	jmp main
 
 
 NUM:
-	in ax, PUERTO_ENTRADA							; leo parametro
-	mov dx, [puertoLog]						;
-	out dx, ax	
-	mov BX,[tope]							; out en bitacora del parametro leido
-	cmp BX, 62	; if (tope == stack_size)
-	jz fullStack						
+	in AX, ENTRADA							; AX = parametro
+	mov DX, [puertoLog]						
+	out DX, AX								; Muestro en DX parametro
+	mov BX,[tope]							; BX = tope
+	cmp BX, 62								; tope = maxima cantidad de elementos?
+	jz fullStack							
 	call pushStack	
-	jmp exito						; se agrega el parametro al stack
+	jmp exito								; Mensaje de exito
 	
 	
 fullStack:
-	mov ax, 4
-	out dx, ax								; out 4 : desbordamiento de la pila
+	mov AX, 4
+	out DX, AX								; Muestro codigo 4 asociado al desbordamiento del stack
 	jmp main
 
 exito:
-	mov dx, [puertoLog]
-	mov ax, 16
-	out dx, ax	
+	mov DX, [puertoLog]
+	mov AX, 16								; Muestro codigo 16 asociado al exito en la operacion 
+	out DX, AX	
 	jmp main
 
 
 PORT:					
-	in ax, PUERTO_ENTRADA				; parametro = IN(ENTRADA)
-	mov dx, [puertoLog]			; dx = puertoLog
-	out dx, ax					; out en bitacora del parametro leido
-	mov [puertoSalida], ax		; puertoSalida = parametro
-	mov ax, 16		; ax = 16
-	out dx, ax 					; out 16 : proceso exitoso
+	in AX, ENTRADA							; AX = parametro
+	mov DX, [puertoLog]						
+	out DX, AX								; Muestro en DX parametro
+	mov [puertoSalida], AX					; puertoSalida = parametro
+	mov AX, 16								; Muestro codigo 16 asociado al exito en la operacion
+	out DX, AX 								
 	jmp main
 
-
 LOG:
-						; preservo valores de registros
-	in ax, PUERTO_ENTRADA				; parametro = IN(ENTRADA)
-	mov dx, [puertoLog]			; dx = puertoLog
-	out dx, ax					; out en bitacora del parametro leido
-	mov [puertoLog], ax			; puertoLog = parametro
-	;mov ax, CODIGO_EXITO		; ax = 16
-	;mov dx, [puertoLog] 		; dx = nuevo puerto log
-	;out dx, ax 					; out 16 : proceso exitoso
-	;jmp main
+	in AX, ENTRADA							; AX = parametro
+	mov DX, [puertoLog]			
+	out DX, AX								; Muestro en DX parametro
+	mov [puertoLog], AX						; puertoLog = parametro
 	jmp exito
 
 TOP:
-	mov DI,[tope]
-	cmp DI,0
-	je faltaOperandos
-	sub DI,2
-	mov bx, base
-	mov ax, [bx+di]					; ax = stack[tope]
-	mov dx, [puertoSalida]			; dx = puertoSalida
-	out dx, ax	
+	mov DI,[tope]							; DI = tope
+	cmp DI,0								; tope == 0 ?
+	je faltaOperandos						; si es 0 faltan operandos 
+	sub DI,2								; tope >= 1 pero el dato en byte anterior 
+	mov BX, base				
+	mov AX, [ BX + DI ]						; AX = stack[tope]
+	mov DX, [puertoSalida]					; Muestro en puerto salida el valor cargado 
+	out DX, AX	
 	jmp exito
 	
 
 faltaOperandos:
-	mov dx, [puertoLog]				; dx = puertoLog
-	mov ax, 8	
-	out dx, ax						; out 8 : falta de operandos en la pila
+	mov DX, [puertoLog]						; DX = puertoLog
+	mov AX, 8	
+	out DX, AX								; Muestro codigo 8 asociado a la falta de operandos en el stack
 	jmp main
 
-
 DUMP:
-	mov di, [tope]
-	mov bx, base
-	mov dx, [puertoSalida]	
+	mov DI, [tope]							; DI = tope
+	mov BX, base							; BX = base stack
+	mov DX, [puertoSalida]			
 	jmp while_dump
 		
 while_dump:
-	cmp di, 0
-	jz exito						
-	sub di, 2	
-	mov ax, [bx+di]					
-	out dx, ax						
+	cmp DI, 0								; tope == 0 ?
+	jz exito								; recorri todo stack
+	sub DI, 2						
+	mov AX, [ BX + DI ]						; AX = stack[tope]			
+	out DX, AX								; Muestro en puertoSalida el operando
 	jmp while_dump
 
 DUPP:
-
-	mov dx, [puertoLog]	
-	mov BX,[tope]
-	cmp BX,0
+	mov DX, [puertoLog]						; DX = puertoLog
+	mov BX,[tope]							; BX = tope
+	cmp BX,0								; tope == 0 ?
 	je faltaOperandos
-	sub BX,62
-	jz fullStack					
-	;cmp word ptr [tope], DOBLE_STACK_SIZE	; if (tope == stack_size)
-	;je pila_llena_dup
-	;cmp word ptr [tope], 0					; if (tope == 0)
-	;je pila_vacia_dup	
-	call popStack							; bx = stack[tope]
-;TODO CHANGE POP TO AX 	
-	mov ax, bx								; ax = bx
-	call pushStack							; push(ax)
-	call pushStack							; push(ax)
-	;mov ax, CODIGO_EXITO
-	;out dx, ax								; out 16 : proceso exitoso
-	;jmp fin_dup
+	sub BX,62								; tope == maxima cantidad de elementos?
+	jz fullStack						
+	call popStack							; BX = stack[tope]
+	mov AX, BX						
+	call pushStack							; push con el operando en AX
+	call pushStack							; push el mismo operando ( se preserva el contexto )
 	jmp exito
 
 
 SWAP:
-	mov dx, [puertoLog]
-	mov BX,[tope]
-	cmp BX,0
+	mov DX, [puertoLog]						; DX = puertoLog
+	mov BX,[tope]							; BX = tope
+	cmp BX,0								; tope == 0 ?
 	jz faltaOperandos
-	sub BX,2
+	sub BX,2								; tope == 1?
 	jz eliminarUno
-;TODO: CHANGE BX TO AX 
-	call popStack
-	mov cx, bx			; cx = valor que habia en el tope del stack
-	call popStack		; bx = valor que habia debajo del tope en el stack
-	mov ax, cx 
-	call pushStack		; push(cx) = push(tope)
-	mov ax, bx
-	call pushStack		; push(bx) = push(topeMenosUno)
+	call popStack							; BX = stack[tope]
+	mov CX, BX								; CX = stack[tope]
+	call popStack							; BX = stack[tope - 2]
+	mov AX, CX 								; AX = stack[tope]
+	call pushStack							; push con AX (tope)
+	mov AX, BX								; AX = stack[tope - 2]
+	call pushStack							; push con valor de AX (anterior al tope ahora en tope)
 	jmp exito
 	
 	
 eliminarUno:
-
 	call popStack			
 	jmp faltaOperandos
 
 
 OPPOSITE:
-	mov BX,[tope]
+	mov BX,[tope]							; BX = tope
 	cmp BX,0
 	jz faltaOperandos
-	call popStack					
-	neg bx							
-	mov ax, bx						
-	call pushStack					
-	call exito		
+	call popStack							; BX = stack[tope]		
+	neg BX							
+	mov AX, BX								; AX = neg stack[tope]	
+	call pushStack							; push con valor de AX
+	call exito
+
 FACT:
 	mov CX,[tope]			
 	cmp CX, 0			
-	jle faltaOperandos
-	push bx
-	call popStack			
-	mov ax, bx				
-	call factorial			
-	mov ax, bx
-	call pushStack			
+	jle faltaOperandos		
+	push BX
+	call popStack							; BX = stack[tope]	
+	mov AX, BX						
+	call factorial							; llamada al procedimiento factorial con AX = stack[tope]	
+	mov AX, BX
+	call pushStack							;push con AX
 	jmp exito
 
-
 SUM_ALL:
-	mov CX,[tope]
-;TODO CHANGE AX TO BX BECAUSE POP WILL RETURN IN AX
-	xor ax, ax					
+	mov CX,[tope]	
+	xor AX, AX					
 while_sum:
-	cmp CX, 0
-	je end_sum					
-	call popStack				
-	add ax, bx					
-	sub CX,2
+	cmp CX, 0								; tope == 0? termine de sumar 
+	je end_sum						
+	call popStack							; BX = stack[tope]
+	add AX, BX								; AX = contador
+	sub CX, 2
 	jmp while_sum
 end_sum:						
-	call pushStack
+	call pushStack 							; push contador
 	jmp exito
 
 SUM_ONE:
-	mov dx, [puertoLog]
+	mov DX, [puertoLog]
 	mov BX,[tope]
 	cmp BX,0
 	jz faltaOperandos
 	sub BX,2
 	jz eliminarUno
-	
-call popStack	 
-	mov cx, bx 				
-	call popStack			
-	add bx, cx				
-	mov ax, bx
-	call pushStack			
+	call popStack	 						; BX = stack[tope]
+	mov CX, BX 								; CX = stack[tope]
+	call popStack							; BX = stack[tope - 2]
+	add BX, CX								; AX = stack[tope] + stack[tope - 2]
+	mov AX, BX						
+	call pushStack							; push AX 
 	jmp exito
 
-
-SUBB:
-mov dx, [puertoLog]
+SUBB:										; Misma logica 
+	mov DX, [puertoLog]
 	mov BX,[tope]
 	cmp BX,0
 	jz faltaOperandos
 	sub BX,2
 	jz eliminarUno
 	call popStack	 
-	mov cx, bx 				
+	mov CX, BX 				
 	call popStack			
-	sub bx, cx				
-	mov ax, bx
+	sub BX, CX				
+	mov AX, BX
 	call pushStack			
 	jmp exito
 
-MULTIPLY:
+MULTIPLY: 									; Misma logica 
 	mov BX,[tope]
 	cmp BX,0
 	jz faltaOperandos
 	sub BX,2
 	jz eliminarUno
-	xor dx, dx				
+	xor DX, DX				
 	call popStack
-	mov cx, bx				
+	mov CX, BX				
 	call popStack
-	mov ax, bx				
-	imul cx					
+	mov AX, BX				
+	imul CX					
 	call pushStack			
 	jmp exito
 
-
-DIVV:
-mov BX,[tope]
+DIVV:						
+	mov BX,[tope]
 	cmp BX,0
 	jz faltaOperandos
 	sub BX,2
 	jz eliminarUno
-	call popStack	 
-	mov cx, bx 				
-	call popStack			
-	mov ax, bx					
-	cmp ax, 0
-	jl dividendo_negativo
-	xor dx, dx			
-	jmp dividir
-dividendo_negativo:
-	mov dx, 0xffff		
-dividir:
-	idiv cx				
-	call pushStack		
+	call popStack	 						; BX = stack[tope] 
+	mov CX, BX 								; CX = stack[tope] divisor
+	call popStack							; BX = stack[tope - 2] dividendo
+	mov AX, BX								; AX = Dividendo 
+	cmp AX, 0
+	jl Dividendo_negativo			
+	xor DX, DX			
+	jmp Dividir
+Dividendo_negativo:
+	mov DX, 0xffff							; DX = -1
+Dividir:
+	idiv CX				
+	call pushStack							; push con AX con el resultado 
 	jmp exito
-
 
 MOD:
 	mov BX,[tope]
@@ -318,51 +299,50 @@ MOD:
 	jz faltaOperandos
 	sub BX,2
 	jz eliminarUno
-	call popStack
-	mov cx, bx 			
-	call popStack	
-	mov ax, bx			
-	cmp ax, 0
+	call popStack							; BX = stack[tope]
+	mov CX, BX 								; CX = stack[tope]
+	call popStack							; BX = stack[tope - 2]
+	mov AX, BX			
+	cmp AX, 0
 	jl modNegativo
-	xor dx, dx			
-	idiv cx
-	mov ax, dx
-	call pushStack		
+	xor DX, DX			
+	idiv CX
+	mov AX, DX								;division deja el mod en DX
+	call pushStack							;push con AX = MOD 
 	jmp exito
 
 modNegativo:
-	mov dx, 0xffff			
-	idiv cx
-	mov ax, dx
-	call pushStack		
+	mov DX, 0xffff							; DX = -1 misma idea que en div
+	idiv CX
+	mov AX, DX								;division deja el mod en DX
+	call pushStack							;push con AX = MOD 
 	jmp exito	
 	
-
 ANDD:
-	mov BX,[tope]
+	mov BX,[tope]							; Misma logica anterior
 	cmp BX,0
 	jz faltaOperandos
 	sub BX,2
 	jz eliminarUno
 	call popStack	 
-	mov cx, bx 				
+	mov CX, BX 				
 	call popStack			
-	and bx, cx
-	mov ax, bx
+	and BX, CX
+	mov AX, BX
 	call pushStack			
 	jmp exito
 
-ORR:
+ORR:										;Misma logica
 	mov BX,[tope]
 	cmp BX,0
 	jz faltaOperandos
 	sub BX,2
 	jz eliminarUno
 	call popStack
-	mov cx, bx 				
+	mov CX, BX 				
 	call popStack			
-	or bx, cx
-	mov ax, bx
+	or BX, CX
+	mov AX, BX
 	call pushStack			
 	jmp exito
 
@@ -372,66 +352,66 @@ LEFTS:
 	jz faltaOperandos
 	sub BX,2
 	jz eliminarUno
-	call popStack	
-	mov cx, bx 		
-	call popStack	
-	cmp ch, 0		
+	call popStack							;BX = stack[tope]
+	mov CX, BX 								; CX = BX = cantidad a shiftear
+	call popStack							;BX = operando
+	cmp CH, 0								; parte alta != 0 ?
 	jne cargarCero
-	sal bx, cl		
-	mov ax, bx
+	sal BX, CL		
+	mov AX, BX
 	call pushStack	
 	jmp exito
 
 cargarCero:
-	mov ax, 0		
-	call pushStack
+	mov AX, 0								; Numero mayor a 16 entonces AX =  0
+	call pushStack							;push con AX
 	jmp exito
 
-	
 
-
-RIGHTS:		
+RIGHTS:										;Misma logica cargando -1 en operando si la cantidad es mayor a 16
 	mov BX,[tope]
 	cmp BX,0
 	jz faltaOperandos
 	sub BX,2
 	jz eliminarUno
 	call popStack	
-	mov cx, bx 		
+	mov CX, BX 		
 	call popStack	
-	cmp ch, 0		
+	cmp CH, 0		
 	jne cargarMenosUno
-	sar bx, cl		
-	mov ax, bx
+	sar BX, CL		
+	mov AX, BX
 	call pushStack	
 	jmp exito
 	
 
 cargarMenosUno:
-	mov ax, -1		
+	mov AX, -1		
 	call pushStack
 	jmp exito
 
 CLEAR:				
-	mov word ptr [tope], 0				; tope = 0 (borrado logico)
+	mov word ptr [tope], 0				; Borrado logico dejo el tope en 0
 	jmp exito	
 	jmp main
 
-HALT:
-	mov ax, 16
-	mov dx, [puertoLog]
-	out dx, ax							
-endLoop:
+HALT:									
+	mov AX, 16							;Muestro mensaje de exito 
+	mov DX, [puertoLog]
+	out DX, AX							
+endLoop:								;Loop infinito
 	jmp endLoop
 
 
+;;---------  Funciones Auxiliares  -----------------------------
+;;--------------------------------------------------------------
 
 pushStack proc
 	push BX
 	push DI						
 	mov DI, [tope]	
 	mov BX, base
-	mov [BX + DI],AX 				
+	mov [BX + DI],AX 		; stack[tope] = AX			
 	ADD DI,2
 	mov [tope], DI
 	pop DI
@@ -439,50 +419,43 @@ pushStack proc
 	ret
 pushStack endp
 
-; retorna el resultado en el registro BX
+
 popStack proc
 	push DI
 	mov DI,[tope]
 	sub DI,2	
 	mov [tope],DI
 	mov BX, base
-	mov BX, [BX+DI]			; bx = stack[tope]
+	mov BX, [BX+DI]			; BX = stack[tope]
 	pop DI
 	ret
 popStack endp
 
 
 factorial proc
-	cmp ax, 0			; n==0 ?
-	je paso_base		; return 1
-	dec ax				; n' = n-1
-	call factorial		; ax = factorial(n')
-	inc ax
-	mov cx, ax
-	mul bx				; dx::ax = ax * bx
-	mov bx, ax
-	mov ax, cx
-	jmp fin_factorial	; return n * factorial(n')
+	cmp AX, 0			
+	je paso_base		
+	dec AX				
+	call factorial		
+	inc AX
+	mov CX, AX
+	mul BX				
+	mov BX, AX
+	mov AX, CX
+	jmp fin_factorial	
 paso_base:
-	mov bx, 1			
+	mov BX, 1			
 fin_factorial:
 	ret
 factorial endp
 
 .ports
-10:1, -8, 1, 4097, 19, 4, 255
+ENTRADA:1, -8, 1, 4097, 19, 4, 255
 
 ;10: 16, 1, 2, 16, 1, 2, 1, 3, 16, 4, 1, -1, 16, 4, 1, 1, 16, 4, 254, 1, 1, 1, 2, 1, 3, 1, 4, 1, 5, 1, 6, 1, 7, 1, 8, 1, 9, 1, 10, 1, 11, 1, 12, 1, 13, 1, 14, 1, 15, 1, 16, 1, 17, 1, 18, 1, 19, 1, 20, 1, 21, 1, 22, 1, 23, 1, 24, 1, 25, 1, 26, 1, 27, 1, 28, 1, 29, 1, 30, 1, 31, 16, 2, 15, 5, 255
-
 ;10: 1,1, 1,2, 1,3, 1,4, 1,5, 1,1, 1,9, 1,8, 1,-1400, 1,10, 1,11, 1,12, 1,13, 11, 4, 12, 4, 13, 4, 14, 4, 15, 4, 16, 4, 17, 4, 18, 4, 7, 4, 19, 4, 10, 4, 8, 4, 6, 5, 254, 255 
 ;10:1, -8, 1, 4097, 19, 4, 255
-
-
- ;PUERTO_ENTRADA: 11, 6, 1,1234, 7, 1,4321, 5, 12, 5, 9, 5, 1,-5, 8, 16, 1,1, 1,2, 1,3, 1,4, 1,5, 1,6, 1,7, 1,8, 1,9, 1,10, 1,11, 1,12, 1,13, 1,14, 1,15, 1,16, 1,17, 1,18, 1,19, 1,20, 1,21, 1,22, 1,23, 1,24, 1,24, 1,26, 1,27, 1,28, 1,29, 1,30, 1,31, 1,32, 1,33, 5, 255
-
-
-
+;PUERTO_ENTRADA: 11, 6, 1,1234, 7, 1,4321, 5, 12, 5, 9, 5, 1,-5, 8, 16, 1,1, 1,2, 1,3, 1,4, 1,5, 1,6, 1,7, 1,8, 1,9, 1,10, 1,11, 1,12, 1,13, 1,14, 1,15, 1,16, 1,17, 1,18, 1,19, 1,20, 1,21, 1,22, 1,23, 1,24, 1,24, 1,26, 1,27, 1,28, 1,29, 1,30, 1,31, 1,32, 1,33, 5, 255
 ;PUERTO_ENTRADA: 1,2,1,4,7,5,255
 ;PUERTO_ENTRADA: 1,1, 1,2, 1,3, 1,4,1,5,1,6,1,7,1,8,1,9,1,10,1,11,1,12,1,13,1,14,1,15,1,16,1,17,1,18,1,19,1,220,1,21,1,22,1,23,1,24,1,25,1,26,1,27,1,28,1,29,1,30,4,6,5,255
-
 
